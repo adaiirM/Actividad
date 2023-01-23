4c3ff65df87e6712ecd889323d825c95c04a7eb9
Adair M <100882820+adaiirM@users.noreply.github.com>
2023-01-23 15:10:50 -0600

    Delete src directory

--- src/Interfaz/InterfazInsertar.java
+++
@@ -1,8 +0,0 @@
-package Interfaz;
-
-import javax.swing.*;
-
-public class InterfazInsertar {
-    private JTextField textField1;
-    private JTextField textField2;
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import Interfaz.InterfazEmpresa;
-
-public class Main {
-
-    public static void main(String[] args) {
-        //Llama a la interfaz principal
-        new InterfazEmpresa();
-    }
-}
